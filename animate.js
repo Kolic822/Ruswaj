04ef013429b2d43b37f0982d5696c015ff2b3f45
Kolic822 <33804549+Kolic822@users.noreply.github.com>
2020-05-02 15:28:59 +0200

    Add files via upload

--- animate.js
+++ animate.js
@@ -1,15 +1,80 @@
-function showClanovi(){
+
 const clanovi = document.getElementsByClassName("clanovi")[0];
 const onama = document.getElementsByClassName("o-nama")[0];
+const kontakt = document.getElementsByClassName("kontakt")[0];
+const repertoar = document.getElementsByClassName("repertoar")[0];
+const events = document.getElementsByClassName("events")[0];
+const galerija = document.getElementsByClassName("galerija")[0];
 
-    clanovi.style.display ="block";
-    onama.style.display ="none";
+
+function showOnama() {
+    onama.style.display = "block";
+    clanovi.style.display = "none";
+    kontakt.style.display = "none";
+    repertoar.style.display = "none";
+    galerija.style.display = "none";
+    events.style.display = "none";
 }
 
-function showOnama(){
-    const clanovi = document.getElementsByClassName("clanovi")[0];
-    const onama = document.getElementsByClassName("o-nama")[0];
-    
-        clanovi.style.display ="none";
-        onama.style.display ="block";
-    }
+function showClanovi() {
+    clanovi.style.display = "block";
+    onama.style.display = "none";
+    kontakt.style.display = "none";
+    repertoar.style.display = "none";
+    galerija.style.display = "none";
+    events.style.display = "none";
+}
+
+function showRepertoar() {
+    repertoar.style.display = "block";
+    kontakt.style.display = "none";
+    onama.style.display = "none";
+    clanovi.style.display = "none";
+    galerija.style.display = "none";
+    events.style.display = "none";
+}
+
+function showEvents() {
+    events.style.display = "block";
+    kontakt.style.display = "none";
+    onama.style.display = "none";
+    clanovi.style.display = "none";
+    galerija.style.display = "none";
+    repertoar.style.display = "none";
+}
+
+function showGalerija() {
+    galerija.style.display = "block";
+    kontakt.style.display = "none";
+    onama.style.display = "none";
+    clanovi.style.display = "none";
+    repertoar.style.display = "none";
+    events.style.display = "none";
+}
+
+function showKontakt() {
+    kontakt.style.display = "block";
+    onama.style.display = "none";
+    clanovi.style.display = "none";
+    repertoar.style.display = "none";
+    galerija.style.display = "none";
+    events.style.display = "none";
+}
+
+
+
+// prek JS-a napravi HTML element
+// Za svaki html element append item u onaj images wrapper
+
+function toggleOverlay(imgUrl) {
+    document.querySelector('.showImg').classList.add('showOverlay');
+
+    document.getElementById('presentation').src = imgUrl;
+    console.log('FOOO', imgUrl);
+}
+
+// Close overlay
+function closeImg() {
+    document.querySelector('.showImg').classList.remove('showOverlay');
+    document.getElementById('presentation').src = '';
+}
